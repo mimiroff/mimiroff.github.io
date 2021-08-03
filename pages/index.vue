853cef264a9575f277efc9bb6928af05bedb569e
Ilya Barashkov <ibarashkov@list.ru>
2021-08-03 13:20:07 +0300

    date fix

--- pages/index.vue
+++ pages/index.vue
@@ -78,7 +78,7 @@ export default {
         }
       ],
       aboutMe: {
-        left: `Hi! I'm Ilya Barashkov, a software engineer in web development. I have loved coding for decades as a hobby. Now I love it even more as a profession. Since 2019 I'm working as a full-stack developer creating nicely and logically structured UI and API, building the reliable and efficient backend.`,
+        left: `Hi! I'm Ilya Barashkov, a software engineer in web development. I have loved coding for decades as a hobby. Now I love it even more as a profession. Since 2018 I'm working as a full-stack developer creating nicely and logically structured UI and API, building the reliable and efficient backend.`,
         right: `My way into the profession was fast and effective because I was lucky to have wonderful mentors. Now, as an act of acknowledgment, I am helping newcomers to get into web development with mentorship by myself and I'm trying my best to match my teachers.`
       }
     }
